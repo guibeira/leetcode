47e84eea8eacc0b08a7431f71d0ead3927a44779
guilhermevbeira <guilherme.vieira.beira@gmail.com>
2023-12-21 14:07:44 -0300

    Update solution to use two pointers

--- 125.valid-palindrome-python3.py
+++ 125.valid-palindrome-python3.py
@@ -3,14 +3,26 @@ class Solution:
     def isPalindrome(self, s: str) -> bool:
         s = s.lower()
         s = list(filter(str.isalnum, s))
-        r = list(s)
-        r.reverse()
+        # r = list(s)
+        # r.reverse()
 
-        while s:
-            first = s.pop()
-            last = r.pop()
-            if first != last:
+        # while s:
+        #     first = s.pop()
+        #     last = r.pop()
+        #     if first != last:
+        #         return False
+        # return True
+        r_index = len(s) - 1
+        l_index = 0
+
+        for _ in range(len(s)):
+            if s[l_index] != s[r_index]:
                 return False
+            # case where we cover all the letters
+            if l_index > r_index:
+                break
+            l_index += 1
+            r_index -= 1
         return True
 
 
